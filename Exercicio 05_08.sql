7b4f3e4f25aa9ba92b12a08b6e17df6579b4b050
vinis1910 <viniciusgabrielmartins79@gmail.com>
2022-08-20 02:09:01 -0300

    Atualização sql dia 31

--- Exercicio 05_08.sql
+++ Exercicio 05_08.sql
@@ -1,50 +1,46 @@
-create table pedido(
-	i_codiddoPedido_pedido INT(4),
-    t_prazoEntrega_pedido DATETIME,
-    t_dataPedido_pedido DATETIME,
-    i_codCliente_pedido INT(4),
-    i_codVendedor_pedido INT(4)
-);
-alter table pedido modify column i_codiddoPedido_pedido INT PRIMARY KEY;
-alter table pedido add constraint fk_codVendedor foreign key (i_codVendedor_pedido) references vendedor (i_codVendedor_vendedor);
-alter table pedido add constraint fk_codCliente foreign key (i_codCliente_pedido) references cliente (i_codCliente_cliente);
 
 create table vendedor(
-	i_codVendedor_vendedor INT,
+	i_codVendedor_vendedor INT UNSIGNED not null  AUTO_INCREMENT,
     s_nome_vendedor VARCHAR(45),
-    d_salarioFixo_vendedor DECIMAL(10,2),
-    e_faixaComissao_vendedor INT
+    dec_salarioFixo_vendedor DECIMAL(10,2),
+    en_faixaComissao_vendedor ENUM('A', 'B', 'C', 'D'),
+    PRIMARY KEY(i_codVendedor_vendedor)
 );
 
-alter table vendedor modify column i_codVendedor_vendedor INT PRIMARY KEY;
-
-
-
 create table cliente(
-	i_codCliente_cliente int(4),
+	i_codCliente_cliente INT UNSIGNED AUTO_INCREMENT,
     s_nome_cliente VARCHAR(45),
     s_endereco_cliente VARCHAR(45),
     s_cidade_cliente VARCHAR(45),
     s_cep_cliente VARCHAR(11),
-    s_uf_cliente CHAR(2),
-    s_ie_cliente VARCHAR(12)
+    s_uf_cliente VARCHAR(2),
+    PRIMARY KEY(i_codCliente_cliente)
 );
-
-alter table cliente modify column i_codCliente_cliente INT PRIMARY KEY;
-
-create table itemPedido(
-	i_codItemPedido_itemPedido INT(4),
-    i_codPedido_itemPedido INT(4),
-	i_codProduto_itemPedido INT(4),
-    i_quantidade_itemPedido INT(5)
-);
-alter table itemPedido modify column i_codItemPedido_itemPedido INT PRIMARY KEY;
-alter table itemPedido add constraint fk_codPedido foreign key (i_codPedido_itemPedido) references pedido (i_codiddoPedido_pedido);
-alter table itemPedido add constraint fk_codProduto foreign key (i_codProduto_itemPedido) references produto (i_codProduto_produto);
 
 create table produto(
-	i_codProduto_produto INT(4),
+	i_codProduto_produto INT UNSIGNED AUTO_INCREMENT,
     s_descricao_produto VARCHAR(45),
-    d_valorUnitario_produto DECIMAL(10,2)
+    dec_valorUnitario DECIMAL(10,2),
+    PRIMARY KEY(i_codProduto_produto)
 );
-alter table produto modify column i_codProduto_produto INT PRIMARY KEY;
+
+create table itemPedido(
+	i_codItemPedido_itemPedido INT UNSIGNED AUTO_INCREMENT,
+	i_codPedido_itemPedido INT UNSIGNED,
+    i_codProduto_itemPedido INT UNSIGNED,
+	i_quantidade_itemPedido INT UNSIGNED,
+    PRIMARY KEY(i_codItemPedido_itemPedido),
+    FOREIGN KEY(i_codPedido_itemPedido) REFERENCES pedido(i_codPedido_pedido),
+    FOREIGN KEY(i_codProduto_itemPedido) REFERENCES produto(i_codProduto_produto)
+);
+
+create table pedido(
+	i_codPedido_pedido INT UNSIGNED not null AUTO_INCREMENT,
+    d_prazoEntrega_pedido DATE,
+    d_dataPedido_pedido DATE,
+    i_codCliente_pedido INT unsigned,
+    i_codVendedor_pedido INT unsigned,
+    primary key(i_codPedido_pedido),
+    foreign key (i_codVendedor_pedido) references vendedor(i_codVendedor_vendedor),
+    FOREIGN KEY (i_codVendedor_pedido) references cliente(i_codCliente_cliente)
+);
